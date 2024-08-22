d2857476a79d3e5f1898e1cd1a8210ee0c93ce33
lizzietorp <lizzie.torp@gmail.com>
2024-08-22 10:49:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,26 +13,23 @@ function stop()
     //these functions make it so the button is disabled when you click on it and the other button becomes enabled
 }
 
+//function to start interval
 function intervalStart()
 {
-    
     var image = document.getElementById("memeImage");
-    var change = 2;
- 
-    intervalID = setInterval(function()
+    var change = 0; //starting position
+
+    intervalID = setInterval(function() 
     {
-        image.style.left = change+"px";
+       image.style.left = change+"px";
        //plus means to add to get the img to move
         change+=2;
  
-    },500); // 500 means .5 seconds
+    },500); //means .5 seconds interval
 
 }
+//function to stop interval
 function intervalStop()
 {
-    function intervalStop()
-    {
-        clearInterval(intervalID);
-        clearInterval
-    }
+    clearInterval(intervalID); // Clear the interval
 }
